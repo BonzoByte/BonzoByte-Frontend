a6e25422c7ee3d8335e5053276b478f7955839c3
BonzoByte <bonzobyte@gmail.com>
2026-02-13 10:28:30 +0100

    reformating code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: true,
-  
+
     apiUrl: 'https://bonzobyte-backend.onrender.com/api',
-  
+
     archivesMode: 'api',
     apiBase: 'https://bonzobyte-backend.onrender.com/api/archives',
     dailyStaticBase: '',
-  };
-  
+};
